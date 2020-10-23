524e6c63b82fe15de26b14b62dd41955f2b3c10c
gamer-28-vidhan <73277612+gamer-28-vidhan@users.noreply.github.com>
2020-10-23 18:08:16 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,143 +1,50 @@
-
-var   monkey, monkey_running
-var ground;
-var banana ,bananaImage, obstacle, obstacleImage
-var FoodGroup, obstacleGroup
-var score=0;
-
-var PLAY = 1;
-var END =0;
-var gamestate = PLAY;
-var time=0;
-
-function preload(){
-  
-  
-  monkey_running =            loadAnimation("monkey_0.png","monkey_1.png","monkey_2.png","monkey_3.png","monkey_4.png","monkey_5.png","monkey_6.png","monkey_7.png","monkey_8.png")
-  
-  bananaImage = loadImage("banana.png");
-  obstacleImage = loadImage("obstacle.png");
- 
-}
+var car, wall;
+var speed,weight;
 
 
 
 function setup() {
-  createCanvas(600,400)
-  
-  monkey = createSprite(200,350);
-  monkey.addAnimation("running",monkey_running);
-  monkey.scale = 0.09;
+  createCanvas(1600,400);
 
-  
-  
-  ground = createSprite(200,380,600,5);
-  
-  ground.x = ground.width/2;
-  ground.shapeColor = "green"
-  
-  obstacleGroup = new Group();
-FoodGroup = new Group();
-  
- 
-  
-  textSize(15);
-  fill("blue")
-  
+  speed = random(55,90)
+  weight = random(400,1500)
+
+  car = createSprite(50,200,50,50)
+  wall = createSprite(1500,200,60,height/2);
+
+  car.velocityX = speed;
+
+
 }
-
 
 function draw() {
-  background("lightgreen");
-  text("survived for : "+time, 300,50);
- 
+  background(255,255,255);  
   
-  
-  if(gamestate === PLAY){
-    monkey.visible = true;
-   ground.visible = true; 
-  spawnFood();
- 
-    
-  ground.velocityX = -(5 + 3*score/3);
-  
-  if(ground.x < 350){
-   ground.x = ground.width/2; 
-  }       
-  
-  if(keyDown("space") && monkey.y>349){
-     
-    monkey.velocityY = -14;
-    
-     }
+  if(wall.x - car.x > (car.width + wall.width)/2){
 
-  monkey.velocityY = monkey.velocityY + 0.8;
-  
-  monkey.collide(ground);
-    
-    time =   Math.round(frameCount/100)
-    
-  
-  if(FoodGroup.isTouching(monkey)){
-   FoodGroup.destroyEach(); 
-    score = score +1;
+car.velocityX = 0;
+var deformation = 0.5*weight*speed*speed/22500
   }
-     drawSprites();
-  text("Bananas:"+score, 480,50)
-  
-  if(obstacleGroup.isTouching(monkey)){
-    gamestate = END;
-    
-     FoodGroup.destroyEach(); 
-     obstacleGroup.destroyEach(); 
-  } 
-   
-  }
-  
-  if(gamestate === END){
-    background("black")
-    text("press p to play again", 220,260)
-   text("GAME OVER !", 250,200);
-      
-    if(keyDown("p") && gamestate===END){
-   gamestate = PLAY; 
-      monkey.addAnimation("running",monkey_running)
-  time=0
-  score=0;
-       
-  }
-  
-  
-  }
-    
-   
-  
-   
-  
 
+if(deformation > 180){
+
+  car.shapecolor = "red";
+}
+
+if(deformation < 180 && deformation > 100){
+
+  car.shapeColor = "yellow";
+}
+
+if(deformation < 100){
+
+  car.shapeColor = "green";
 }
 
 
+  
+  
+  
+  drawSprites();
 
-function spawnFood() {
- 
-  if(frameCount% 160 === 0){
-   banana = createSprite(600,Math.round(random(200,300)))
-  banana.addImage(bananaImage);
-  banana.scale = 0.07;
-      FoodGroup.add(banana)
-    banana.velocityX = -(5 + 2*score/3);
-    obstacle = createSprite(600,360)
-    obstacle.addImage(obstacleImage);
-    obstacle.scale = 0.08;
-    obstacle.velocityX = -(5 + 2*score/3);
-    obstacleGroup.add(obstacle);
-} 
-  }
-
-
-
-
-
-
-
+}
